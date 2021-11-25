40e8f24d48741fe5a1b0a3b51e1614e82b9ca79b
pallav1998 <jain.pallav1998@gmail.com>
2021-11-25 13:45:21 +0530

    all features added

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const progress = document.getElementById("progress");
+const outer_div = document.getElementById("outer_div");
+
 let now_playing = document.querySelector(".now-playing");
 let track_name = document.querySelector(".track-name");
 
@@ -8,8 +13,29 @@ let prev_btn = document.querySelector(".prev-track");
 let seek_slider = document.querySelector(".seek_slider");
 let volume_slider = document.querySelector(".volume_slider");
 
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+
 let track_index = 0;
 let isPlaying = false;
+let updateTimer;
+
+//Canvas Logic
+function CanvasBars() {
+  canvas.width = 1000;
+  canvas.height = 200;
+  context.fillStyle = "brown";
+  let arr = Array.from({ length: 1000 }, () => Math.floor(Math.random() * 100));
+  for (let i = 0; i < arr.length; i += 9) {
+    arr[i] = -arr[i];
+    // i++;
+  }
+  console.log(arr);
+  for (let i = 0; i < arr.length; i++) {
+    context.fillRect(i, 100, 8, -arr[i]);
+    i = i + 5;
+  }
+}
 
 // Create new audio element
 let curr_track = document.createElement("audio");
@@ -30,8 +56,23 @@ let track_list = [
   },
 ];
 
-function loadTrack(track_index) {
+function random_bg_color() {
+  // Get a number between 64 to 256 (for getting lighter colors)
+  let red = Math.floor(Math.random() * 256) + 64;
+  let green = Math.floor(Math.random() * 256) + 64;
+  let blue = Math.floor(Math.random() * 256) + 64;
 
+  // Construct a color withe the given values
+  let bgColor = "rgb(" + red + "," + green + "," + blue + ")";
+
+  // Set the background to that color
+  document.body.style.backgroundColor = bgColor;
+}
+
+function loadTrack(track_index) {
+  CanvasBars();
+  clearInterval(updateTimer);
+  resetValues();
 
   now_playing.innerText =
     "PLAYING " + (track_index + 1) + " OF " + track_list.length;
@@ -40,11 +81,20 @@ function loadTrack(track_index) {
   curr_track.src = track_list[track_index].path;
   console.log("curr_track.src:", curr_track.src);
   curr_track.load();
+
+  updateTimer = setInterval(seekUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
+  random_bg_color();
 }
 
 // Load the first track in the tracklist
 loadTrack(track_index);
 
+function resetValues() {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "99:99";
+  seek_slider.value = 0;
+}
 
 function playpauseTrack() {
   if (!isPlaying) {
@@ -86,4 +136,59 @@ function prevTrack() {
   playTrack();
 }
 
+function seekTo() {
+  let seek_to = curr_track.duration * (seek_slider.value / 100);
+  curr_track.currentTime = seek_to;
+}
 
+function setVolume() {
+  curr_track.volume = volume_slider.value / 100;
+}
+
+function seekUpdate() {
+  let seekPosition = 0;
+
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+    // console.log("seekPosition:", seekPosition);
+
+    seek_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(
+      curr_track.currentTime - currentMinutes * 60
+    );
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(
+      curr_track.duration - durationMinutes * 60
+    );
+
+    if (currentSeconds < 10) {
+      currentSeconds = "0" + currentSeconds;
+    }
+    if (durationSeconds < 10) {
+      durationSeconds = "0" + durationSeconds;
+    }
+    if (currentMinutes < 10) {
+      currentMinutes = "0" + currentMinutes;
+    }
+    if (durationMinutes < 10) {
+      durationMinutes = "0" + durationMinutes;
+    }
+    progress.style.width = `${Number(seekPosition)}%`;
+
+    curr_time.textContent = `${currentMinutes}:${currentSeconds}`;
+    total_duration.textContent = `${durationMinutes}:${durationSeconds}`;
+  }
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = curr_track.duration;
+
+  curr_track.currentTime = (clickX / width) * duration;
+  // console.log("curr_track.currentTime:", curr_track.currentTime);
+}
+
+outer_div.addEventListener("click", setProgress);
